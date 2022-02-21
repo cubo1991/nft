2e484a823f37642b0ac6177e19acc263fc9bcbb6
cubo1991 <adavidlopezmathez@gmail.com>
2022-02-21 20:33:05 -0300

    21/02/2022

--- java.js
+++ java.js
@@ -1,3 +1,39 @@
+
+
+window.addEventListener('scroll', function(){
+    let animacion1 = document.getElementById('animado1');
+    let animacion2 = document.getElementById('animado2');
+    let animacion3 = document.getElementById('animado3');
+    let animacion4 = document.getElementById('animado4');
+    let posicionObj1 = animacion1.getBoundingClientRect().top;
+    let posicionObj2 = animacion2.getBoundingClientRect().top;
+    let posicionObj3 = animacion3.getBoundingClientRect().top;
+    let posicionObj4 = animacion4.getBoundingClientRect().top;
+    let tamañoDePantalla = window.innerHeight
+    
+    if(posicionObj1 < tamañoDePantalla) {
+        animacion1.style.animation = 'deslizar 1s ease-out'
+        animacion2.style.animation = 'deslizar 1s ease-out'
+        animacion3.style.animation = 'deslizar 1s ease-out'
+        animacion4.style.animation = 'deslizar 1s ease-out'
+        
+        
+    
+    }
+    /*if(posicionObj2 < tamañoDePantalla) {
+        animacion2.style.animation = 'deslizar 1s ease-out'
+    
+    }
+    if(posicionObj3 < tamañoDePantalla) {
+        animacion3.style.animation = 'deslizar 1s ease-out'
+    
+    }
+    if(posicionObj4 < tamañoDePantalla) {
+        animacion4.style.animation = 'deslizar 1s ease-out'
+    
+    }*/
+ })
+
 $(document).ready(function(){
 
 $("#imagen1").show();
